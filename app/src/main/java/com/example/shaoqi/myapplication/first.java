52a643d03cb5f0702172005f6771d0982bc145e6
shaoqi <450864116@qq.com>
2016-03-27 13:50:29 +0800

    Initial commit

---
+++ app/src/main/java/com/example/shaoqi/myapplication/first.java
@@ -0,0 +1,11 @@
+package com.example.shaoqi.myapplication;
+
+/**
+ * Created by shaoqi on 2016/3/8.
+ */
+public class first {
+    public static void main(String args[]) {
+        for (int i = 0; i<2; i++)
+            System.out.println("我爱你");
+    }
+}
